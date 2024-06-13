1ab56e90ab2e847c8d43f06e205dcf8e4c9fb418
Andrew Ayer <agwa@andrewayer.name>
2024-06-13 14:18:12 -0400

    Retry failed downloads

--- proxy/download.go
+++ proxy/download.go
@@ -4,26 +4,46 @@ import (
 	"context"
 	"fmt"
 	"io"
+	mathrand "math/rand/v2"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func download(ctx context.Context, url string) ([]byte, error) {
+	numRetries := 0
+retry:
+	if ctx.Err() != nil {
+		return nil, ctx.Err()
+	}
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
+		if shouldRetry(ctx, numRetries, nil) {
+			numRetries++
+			goto retry
+		}
 		return nil, err
 	}
 	body, err := io.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
+		if shouldRetry(ctx, numRetries, nil) {
+			numRetries++
+			goto retry
+		}
 		return nil, fmt.Errorf("error reading response from %s: %w", url, err)
 	}
 	if resp.StatusCode != 200 {
+		if shouldRetry(ctx, numRetries, resp) {
+			numRetries++
+			goto retry
+		}
 		return nil, fmt.Errorf("%s from %s: %s", resp.Status, url, strings.TrimSpace(string(body)))
 	}
 	return body, nil
@@ -69,3 +89,59 @@ func formatTileIndex(tile uint64) string {
 	}
 	return str
 }
+
+func shouldRetry(ctx context.Context, numRetries int, resp *http.Response) bool {
+	if numRetries == maxRetries {
+		return false
+	}
+	if resp != nil && !isRetryableStatusCode(resp.StatusCode) {
+		return false
+	}
+	delay := baseRetryDelay * (1 << numRetries)
+	if delay > maxRetryDelay {
+		delay = maxRetryDelay
+	}
+	delay += randomDuration(0, delay/2)
+	if retryAfter, hasRetryAfter := getRetryAfter(resp); hasRetryAfter && retryAfter > delay {
+		delay = retryAfter
+	}
+	if deadline, hasDeadline := ctx.Deadline(); hasDeadline && time.Now().Add(delay).After(deadline) {
+		return false
+	}
+	sleep(ctx, delay)
+	return true
+}
+
+func isRetryableStatusCode(code int) bool {
+	return code/100 == 5 || code == http.StatusTooManyRequests
+}
+
+func randomDuration(min, max time.Duration) time.Duration {
+	return min + mathrand.N(max-min+1)
+}
+
+func getRetryAfter(resp *http.Response) (time.Duration, bool) {
+	if resp == nil {
+		return 0, false
+	}
+	seconds, err := strconv.ParseUint(resp.Header.Get("Retry-After"), 10, 16)
+	if err != nil {
+		return 0, false
+	}
+	return time.Duration(seconds) * time.Second, true
+}
+
+func sleep(ctx context.Context, duration time.Duration) {
+	timer := time.NewTimer(duration)
+	defer timer.Stop()
+	select {
+	case <-ctx.Done():
+	case <-timer.C:
+	}
+}
+
+const (
+	baseRetryDelay = 1 * time.Second
+	maxRetryDelay  = 10 * time.Second
+	maxRetries     = 5
+)
