b711b361f542df10b90303010ef1246f28338a3a
Andrew Ayer <agwa@andrewayer.name>
2024-08-15 16:25:12 -0400

    Simplify some code

--- proxy/download.go
+++ proxy/download.go
@@ -113,20 +113,11 @@ func formatTilePath(level string, tile uint64, width uint64) string {
 }
 
 func formatTileIndex(tile uint64) string {
-	str := ""
-	for {
-		rem := tile % 1000
-		tile = tile / 1000
-
-		if str == "" {
-			str = fmt.Sprintf("%03d", rem)
-		} else {
-			str = fmt.Sprintf("x%03d/%s", rem, str)
-		}
-
-		if tile == 0 {
-			break
-		}
+	const base = 1000
+	str := fmt.Sprintf("%03d", tile%base)
+	for tile >= base {
+		tile = tile / base
+		str = fmt.Sprintf("x%03d/%s", tile%base, str)
 	}
 	return str
 }
